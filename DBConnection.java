8d8b3a3affb153bd1e16004049c0c4f9939bc027
SyedaRubbani <syedarubbani@gmail.com>
2025-06-19 00:11:15 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -3,8 +3,8 @@ import java.sql.*;
 public class DBConnection {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/university_db";
-        String user = "root"; // Change if needed
-        String password = "your_password"; // Change your password
+        String user = "root"; 
+        String password = "password"; 
         return DriverManager.getConnection(url, user, password);
     }
 }
